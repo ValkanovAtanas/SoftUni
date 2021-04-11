1fd630a4f338710c8d6d692a05c9d86bfa80df33
ValkanovAtanas <82307178+ValkanovAtanas@users.noreply.github.com>
2021-04-11 11:11:25 +0300

    Add files via upload

---
+++ Java Basics/01 First Steps In Coding - Lab/03.SquareArea.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class SquareArea {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = Integer.parseInt(scanner.nextLine());
+        System.out.println(a * a);
+    }
+}
