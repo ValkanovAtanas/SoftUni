17472bf514b42330d6a77340940bb6f5530f3020
ValkanovAtanas <82307178+ValkanovAtanas@users.noreply.github.com>
2021-04-11 14:45:25 +0300

    Add files via upload

---
+++ Java Fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/07.TheatrePromotion.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+class TheatrePromotion {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String typeOfDay = scanner.nextLine();
+        int age = Integer.parseInt(scanner.nextLine());
+        double price = 0;
+
+        switch (typeOfDay) {
+            case "Weekday":
+                if (age > 64 && age <= 122) {
+                    price = 12;
+                } else if (age > 18 && age <= 64) {
+                    price = 18;
+                } else if (age >= 0 && age <= 18) {
+                    price = 12;
+                } else {
+                    System.out.println("Error!");
+                }
+                break;
+            case "Weekend":
+                if (age > 64 && age <= 122) {
+                    price = 15;
+                } else if (age > 18 && age <= 64) {
+                    price = 20;
+                } else if (age >= 0 && age <= 18) {
+                    price = 15;
+                } else {
+                    System.out.println("Error!");
+                }
+                break;
+            case "Holiday":
+                if (age > 64 && age <= 122) {
+                    price = 10;
+                } else if (age > 18 && age <= 64) {
+                    price = 12;
+                } else if (age >= 0 && age <= 18) {
+                    price = 5;
+                } else {
+                    System.out.println("Error!");
+                }
+                break;
+        }
+        if (price > 0) {
+            System.out.printf("%.0f$", price);
+        }
+    }
+}
