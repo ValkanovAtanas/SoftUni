85bfa350865d73a86065729c126bfd53caff45f6
ValkanovAtanas <82307178+ValkanovAtanas@users.noreply.github.com>
2021-04-11 14:03:12 +0300

    Add files via upload

---
+++ Programming Basics - Exams/28 and 29 March 2020/03.FitnessCard.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+class FitnessCard {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double currentMoney = Double.parseDouble(scanner.nextLine());
+        String sex = scanner.nextLine();
+        int age = Integer.parseInt(scanner.nextLine());
+        String sport = scanner.nextLine();
+        double price = 0;
+
+        switch (sport) {
+            case "Gym":
+                if (sex.equals("m")) {
+                    price = 42;
+                } else {
+                    price = 35;
+                }
+                break;
+            case "Boxing":
+                if (sex.equals("m")) {
+                    price = 41;
+                } else {
+                    price = 37;
+                }
+                break;
+            case "Yoga":
+                if (sex.equals("m")) {
+                    price = 45;
+                } else {
+                    price = 42;
+                }
+                break;
+            case "Zumba":
+                if (sex.equals("m")) {
+                    price = 34;
+                } else {
+                    price = 31;
+                }
+                break;
+            case "Dances":
+                if (sex.equals("m")) {
+                    price = 51;
+                } else {
+                    price = 53;
+                }
+                break;
+            case "Pilates":
+                if (sex.equals("m")) {
+                    price = 39;
+                } else {
+                    price = 37;
+                }
+                break;
+        }
+        if (age <= 19) {
+            price *= 0.8;
+        }
+
+        double difference = Math.abs(currentMoney - price);
+        if (price <= currentMoney) {
+            System.out.printf("You purchased a 1 month pass for %s.", sport);
+        } else {
+            System.out.printf("You don't have enough money! You need $%.2f more.", difference);
+        }
+    }
+}
