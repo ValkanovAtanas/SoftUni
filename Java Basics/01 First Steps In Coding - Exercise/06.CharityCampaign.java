39b7915134abf3f64195beeba0310c2415c92bd7
ValkanovAtanas <82307178+ValkanovAtanas@users.noreply.github.com>
2021-04-11 11:37:06 +0300

    Add files via upload

---
+++ Java Basics/01 First Steps In Coding - Exercise/06.CharityCampaign.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class VacationBooksList {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int days = Integer.parseInt(scanner.nextLine());
+        int pastryChefs = Integer.parseInt(scanner.nextLine());
+        int cakes = Integer.parseInt(scanner.nextLine());
+        int waffles = Integer.parseInt(scanner.nextLine());
+        int pancakes = Integer.parseInt(scanner.nextLine());
+        double cakesPrice = cakes * 45;
+        double wafflesPrice = waffles * 5.8;
+        double pancakesPrice = pancakes * 3.2;
+        double totalPrice = (cakesPrice + wafflesPrice + pancakesPrice) * days * pastryChefs;
+        double finalSum = totalPrice * 7 / 8;
+        System.out.printf("%.2f", finalSum);
+    }
+}
