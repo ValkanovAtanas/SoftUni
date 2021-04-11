abf7708d87184ae57cd3fee901c9ce4434365c65
ValkanovAtanas <82307178+ValkanovAtanas@users.noreply.github.com>
2021-04-11 12:42:06 +0300

    Add files via upload

---
+++ Java Basics/04 For Loop - Lab/11.CleverLily.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class NumberSequence {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int age = Integer.parseInt(scanner.nextLine());
+        double washingMachinePrice = Double.parseDouble(scanner.nextLine());
+        int toyPrice = Integer.parseInt(scanner.nextLine());
+        int toyCounter = 0;
+        double moneyForBirthday = 0;
+        double moneyForBirthdaySum = 0;
+
+        for (int i = 1; i <= age; i++) {
+            if (i % 2 == 1) {
+                toyCounter ++;
+            } else {
+                moneyForBirthday += 10;
+                moneyForBirthdaySum += moneyForBirthday - 1;
+            }
+        }
+        double allToysPrice = toyPrice * toyCounter;
+        double totalMoney = allToysPrice + moneyForBirthdaySum;
+        double difference = Math.abs(totalMoney - washingMachinePrice);
+
+        if (totalMoney >= washingMachinePrice) {
+            System.out.printf("Yes! %.2f", difference);
+        } else {
+            System.out.printf("No! %.2f", difference);
+        }
+    }
+}
