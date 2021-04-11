7393b2bfc9db9f79df4d1b97914a2680fec92e16
ValkanovAtanas <82307178+ValkanovAtanas@users.noreply.github.com>
2021-04-11 13:21:52 +0300

    Add files via upload

---
+++ Java Basics/05 While Loop - Lab/04.Sequence2kPlus1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Sequence2kPlus1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        int sum = 1;
+
+        while (sum <= n) {
+            System.out.println(sum);
+            sum = sum * 2 + 1;
+
+        }
+    }
+}
